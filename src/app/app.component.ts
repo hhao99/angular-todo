047f4a3bdb849b2159579e61ebc68b7e82964849
hhao99 <hhao99@163.com>
2018-12-04 14:19:00 +0800

    todo step1

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Todo } from './todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Todo MVC App';
+  todo: Todo = new Todo()
+}
